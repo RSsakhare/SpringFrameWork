d97792e998f390bcd45a996681eaff249a334492
RSsakhare <rushikeshsakhare2906@gmail.com>
2025-01-16 15:13:53 +0530

    Spring Program by creating bean factory object

---
+++ SpringDemoUsingBeanFactory(UsingXml)/src/Student.java
@@ -0,0 +1,36 @@
+
+public class Student {
+	
+	int sid;
+	String sname;
+	static int cnt;
+	public Student() {
+		sid=++cnt;
+	}
+	public Student( String sname) {
+		super();
+		this.sid = ++cnt;
+		this.sname = sname;
+	}
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", sname=" + sname + "]";
+	}
+	public void display() {
+		
+		System.out.println("Student Id :"+sid);
+		System.out.println("Student Name :"+sname);
+	}
+}
